96f3bb8a61b13e1b9596a165f641c19ee0df4185
Justin Hued <huedjustin@gmail.com>
2022-07-19 15:13:39 -0400

    initialized react

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <div className="header-options">
+        <span>About</span>
+        <span>Projects</span>
+        <span>Contact Me</span>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Justin Hued</h1>
+    </div>
+  );
+};
+
+export default Home;
