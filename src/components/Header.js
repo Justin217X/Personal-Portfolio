9a15d2aad0163a83c896d4f244e50e16c6cbaf4a
Justin Hued <huedjustin@gmail.com>
2022-07-20 16:11:48 -0400

    created Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,20 @@
 import React from "react";
+import "../styles/Header.css";
 
 const Header = () => {
   return (
     <div className="header-container">
-      <div className="header-options">
-        <span>About</span>
-        <span>Projects</span>
-        <span>Contact Me</span>
+      <div className="header-left"></div>
+      <div className="header-right">
+        <div className="header-option">
+          <a href="#About"> About </a>
+        </div>
+        <div className="header-option">
+          <a href="#Projects"> Projects </a>
+        </div>
+        <div className="header-option">
+          <a href="#ContactMe"> Contact Me </a>
+        </div>
       </div>
     </div>
   );
